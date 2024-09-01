2ded050641f04ce34c08e497bb3f4caa385ceb84
Gibs <gibsramen@gmail.com>
2024-09-01 09:07:49 -0700

    Add aerial to telescope

--- lua/gibs/plugins/telescope.lua
+++ lua/gibs/plugins/telescope.lua
@@ -10,5 +10,21 @@ return {
         keymap.set("n", "<leader>rg", builtin.live_grep)
         keymap.set("n", "<leader>fd", builtin.lsp_document_symbols)
         keymap.set("n", "<leader>fb", builtin.buffers)
+
+        extensions = {
+            aerial = {
+                -- How to format the symbols
+                format_symbol = function(symbol_path, filetype)
+                    if filetype == "json" or filetype == "yaml" then
+                        return table.concat(symbol_path, ".")
+                    else
+                        return symbol_path[#symbol_path]
+                    end
+                end,
+                -- Available modes: symbols, lines, both
+                show_columns = "both",
+            },
+        }
+        keymap.set("n", "<leader>ft", require("telescope").extensions.aerial.aerial)
     end,
 }
