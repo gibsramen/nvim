db7ebe2b25ac9dc35257201e5ba1988e1104f2d9
Gibs <gibsramen@gmail.com>
2024-09-01 09:03:12 -0700

    Update aerial

--- lua/gibs/plugins/aerial.lua
+++ lua/gibs/plugins/aerial.lua
@@ -4,15 +4,21 @@ return {
         local aerial = require("aerial")
 
         aerial.setup({
-          -- optionally use on_attach to set keymaps when aerial has attached to a buffer
-          on_attach = function(bufnr)
-            -- Jump forwards/backwards with '{' and '}'
-            vim.keymap.set("n", "<leader>{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
-            vim.keymap.set("n", "<leader>}", "<cmd>AerialNext<CR>", { buffer = bufnr })
-            vim.keymap.set("n", "<leader>ao", "<cmd>AerialOpen<CR>")
-            vim.keymap.set("n", "<leader>ac", "<cmd>AerialClose<CR>")
-            vim.keymap.set("n", "<leader>an", "<cmd>AerialNavToggle<CR>")
-          end,
+            -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+            on_attach = function(bufnr)
+                -- Jump forwards/backwards with '{' and '}'
+                vim.keymap.set("n", "<leader>{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+                vim.keymap.set("n", "<leader>}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+                vim.keymap.set("n", "<leader>ao", "<cmd>AerialOpen<CR>")
+                vim.keymap.set("n", "<leader>ac", "<cmd>AerialClose<CR>")
+                vim.keymap.set("n", "<leader>an", "<cmd>AerialNavToggle<CR>")
+            end,
+            layout = {
+                max_width = { 30, 0.2 },
+                min_width = { 30, 0.2 },
+                default_direction = "prefer_left",
+                placement = "edge",
+            },
         })
     end,
 }
