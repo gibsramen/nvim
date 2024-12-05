ffea69f36ab488858f43e1d654e6f2575ab1ae60
Gibs <gibsramen@gmail.com>
2024-12-04 16:44:33 -0800

    Update leap keys

--- lua/gibs/plugins/leap.lua
+++ lua/gibs/plugins/leap.lua
@@ -1,7 +1,15 @@
 return {
    "ggandor/leap.nvim",
     config = function()
-        require("leap").create_default_mappings()
+        local leap = require("leap")
+
+        leap.create_default_mappings()
+        leap.opts.special_keys = {
+            next_target = '<enter>',
+            prev_target = { '<backspace>', '<tab>' },
+            next_group = ';',
+            prev_group = { '<backspace>', '<tab>' },
+        }
         vim.keymap.set({'n', 'x', 'o'}, 's',  '<Plug>(leap-forward)')
         vim.keymap.set({'n', 'x', 'o'}, 'S',  '<Plug>(leap-backward)')
         vim.keymap.set({'n', 'x', 'o'}, 'gs', '<Plug>(leap-from-window)')
