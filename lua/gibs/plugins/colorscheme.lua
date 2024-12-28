ba2f0effd14a13fdbb593748941bd60bd973de6c
Gibs <gibsramen@gmail.com>
2024-12-28 10:30:17 -0800

    Ghostty updates

--- lua/gibs/plugins/colorscheme.lua
+++ lua/gibs/plugins/colorscheme.lua
@@ -1,6 +1,15 @@
 return {
     "rebelot/kanagawa.nvim",
     config = function()
+        local kanagawa = require("kanagawa")
+        kanagawa.setup({
+            colors = {
+                palette = {
+                    sumiInk3 = "#0f1e27",
+                },
+            },
+        })
+        -- Setup must be called before loading
         vim.cmd.colorscheme("kanagawa-wave")
     end,
 }
--- lua/gibs/set.lua
+++ lua/gibs/set.lua
@@ -1,3 +1,4 @@
+vim.opt.mouse = ""
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.background = dark
@@ -16,3 +17,10 @@ vim.opt.termguicolors = true
 vim.opt.hidden = true
 vim.opt.list = true
 vim.opt.listchars = "tab:|·,trail:·"
+
+-- https://github.com/neovim/neovim/issues/12011#issuecomment-1605258302
+vim.o.guicursor = table.concat({
+  "n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+  "i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+  "r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100"
+}, ",")
