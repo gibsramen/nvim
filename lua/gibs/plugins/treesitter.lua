b34439de4e07e66e9bfb94843b4b6f6d09e3e5a6
Gibraan Rahman <gibraan.rahman@czbiohub.org>
2024-12-31 09:59:42 -0800

    Pin tree-sitter
    I forgot the reason for this but I think it's for compatibility with the
    go LSP

--- lua/gibs/plugins/treesitter.lua
+++ lua/gibs/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    tag = "v0.9.2",
     config = function()
         require("nvim-treesitter.configs").setup({
             ensure_installed = { "lua", "python", "vim", "query", "rust" },
