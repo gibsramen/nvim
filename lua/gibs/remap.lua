cee364471c11ad92d8cbcc369a9cc4c1505fade2
Gibs <gibsramen@gmail.com>
2024-12-31 09:33:01 -0800

    Add border to diagnostic window

--- lua/gibs/remap.lua
+++ lua/gibs/remap.lua
@@ -23,13 +23,13 @@ vim.keymap.set("n", "<leader>d", ":bp|bd #<CR>")
 -- NVIM 0.10 added these default mappings for traversing diagnostics
 -- but they default to non-floating windows.
 vim.keymap.set('n', ']d', function()
-    vim.diagnostic.goto_next({ float = true })
+    vim.diagnostic.goto_next({ float = { border = 'rounded' } })
     end, {
         desc = 'Jump to the next diagnostic with the highest severity',
     }
 )
 vim.keymap.set('n', '[d', function()
-    vim.diagnostic.goto_prev({ float = false })
+    vim.diagnostic.goto_prev({ float = { border = 'rounded' } })
     end, {
         desc = 'Jump to the previous diagnostic with the highest severity',
     }
