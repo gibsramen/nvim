68263c87d781aea7a7707db51d1914185c49892f
Gibs <gibraan.rahman@czbiohub.org>
2025-05-06 13:31:31 -0500

    Update markdown configs to be local

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,5 +1,5 @@
-vim.opt.wrap = true
-vim.keymap.set("n", "h", "gh")
-vim.keymap.set("n", "j", "gj")
-vim.keymap.set("n", "k", "gk")
-vim.keymap.set("n", "l", "gl")
+vim.opt_local.wrap = true
+vim.keymap.set("n", "h", "gh", { buffer = true })
+vim.keymap.set("n", "j", "gj", { buffer = true })
+vim.keymap.set("n", "k", "gk", { buffer = true })
+vim.keymap.set("n", "l", "gl", { buffer = true })
