99ca6bd332b83c4f44bcbe68235a2c65afa74a09
Gibs <gibsramen@gmail.com>
2025-06-11 22:47:53 -0700

    Add underline to diagnostics

--- lua/gibs/highlight.lua
+++ lua/gibs/highlight.lua
@@ -1,3 +1,4 @@
-vim.api.nvim_set_hl(0, 'DiagnosticError', { fg = "#e82424", bg = "#2a2a2a" })
-vim.api.nvim_set_hl(0, 'DiagnosticWarn', { fg = "#ff9e3b", bg = "#2a2a2a" })
+vim.api.nvim_set_hl(0, 'DiagnosticError', { fg = "#e82424", bg = "#2a2a2a", italic = true, underline = true })
+vim.api.nvim_set_hl(0, 'DiagnosticWarn', { fg = "#ff9e3b", bg = "#2a2a2a", italic = true, underline = true })
+vim.api.nvim_set_hl(0, 'DiagnosticInfo', { fg = "#658594", bg = "#2a2a2a", italic = true, underline = true })
 vim.api.nvim_set_hl(0, 'Whitespace', { fg = "#727169" })
