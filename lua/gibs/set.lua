f5e2ef452a07052a328bd08fdac2df1a7d029df4
Gibs <gibsramen@gmail.com>
2025-06-11 18:19:47 -0700

    Use string for background

--- lua/gibs/set.lua
+++ lua/gibs/set.lua
@@ -1,7 +1,7 @@
 vim.opt.mouse = ""
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.background = dark
+vim.opt.background = "dark"
 vim.opt.cursorline = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
