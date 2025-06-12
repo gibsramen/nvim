9131552beea8dd855cf60bdc1a13277fb1461ee0
Gibs <gibsramen@gmail.com>
2025-06-11 18:19:38 -0700

    Update highlight colors

---
+++ lua/gibs/highlight.lua
@@ -0,0 +1,3 @@
+vim.api.nvim_set_hl(0, 'DiagnosticError', { fg = "#e82424", bg = "#2a2a2a" })
+vim.api.nvim_set_hl(0, 'DiagnosticWarn', { fg = "#ff9e3b", bg = "#2a2a2a" })
+vim.api.nvim_set_hl(0, 'Whitespace', { fg = "#727169" })
