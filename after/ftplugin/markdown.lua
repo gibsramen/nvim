6a35cbb1a58a7788a712898d4317738f0f82f6b9
Gibs <gibsramen@gmail.com>
2025-06-26 10:06:03 -0700

    Comment out markdown navigation keybinds
    Only need gj and gk

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,5 +1,5 @@
 vim.opt_local.wrap = true
-vim.keymap.set("n", "h", "gh", { buffer = true })
+-- vim.keymap.set("n", "h", "gh", { buffer = true })
 vim.keymap.set("n", "j", "gj", { buffer = true })
 vim.keymap.set("n", "k", "gk", { buffer = true })
-vim.keymap.set("n", "l", "gl", { buffer = true })
+-- vim.keymap.set("n", "l", "gl", { buffer = true })
